7d5dc5ead6d507ecf39e0249cd7ddc862ab6c8c8
VyVy99 <dinhhavy2406@gmail.com>
2023-08-21 23:23:57 +0700

    code

--- controllers/index.js
+++ controllers/index.js
@@ -1,60 +1,79 @@
 // @ts-nocheck
 
 import Data from "../data/Data.json" assert { type: 'json' };
-import { ProductType } from "../models/index.js";
-
+import { Product, ProductType } from "../models/index.js";
+console.log(Data);
 
 const RenderNavPills = () => {
-    let content = ""
-    const NavPills = Data.navPills
-    for (let index = 0; index < NavPills.length; index++) {
-        const productType = new ProductType()
+  let content = ""
+  const NavPills = Data.navPills
+  console.log(NavPills);
+  for (let index = 0; index < NavPills.length; index++) {
+    const productType = new ProductType()
 
-        for (const key in NavPills[index]) {
+    for (const key in NavPills[index]) {
 
-            productType[key] = NavPills[index][key]// == value
-        }
-        console.log(productType);
+      productType[key] = NavPills[index][key]// == value
+    }
+    console.log(productType);
 
-        if (index === 0) { // index chinh la ob, key la thuoc tinh
-            content += `
+    if (index === 0) { // index chinh la ob, key la thuoc tinh
+      content += `
                     <li class="nav-item">
               <a class="nav-link active" href="#">${productType.showName}</a>
             </li>
         `
-        } else {
-            content += `
+    } else {
+      content += `
                     <li class="nav-item">
               <a class="nav-link " href="#">${productType.showName}</a>
             </li>
         `
-        }
     }
-    document.getElementById("nav-pills").innerHTML = content
+  }
+  document.getElementById("nav-pills").innerHTML = content
 }
 RenderNavPills();
 
-const tryOnProduct = (id) => {
-    console.log(product);
-    const tabPanes = Data.tabPanes;
 
-    for (let i = 0; i < tabPanes.length; i++) {
-        if (tabPanes[i].id === id) {
-            const { id, type, name, desc, imgSrc_jpg, imgSrc_png } = tabPanes[i];
-            product = new Product(id, type, name, desc, imgSrc_jpg, imgSrc_png);
-            break;
-        }
+const chooseItem=()=>{
+  let { tabPanes, navPills } = objectData;
+  console.log(tabPanes);
+  for (let tab of tabPanes) {
+    for (let pill of navPills) {
+  
+      }
+    }
+  }
+
+
+
+
+
+
+const tryOnProduct = (id) => {
+  console.log(product);
+  const tabPanes = Data.tabPanes;
+
+  for (let i = 0; i < tabPanes.length; i++) {
+    if (tabPanes[i].id === id) {
+      const { id, type, name, desc, imgSrc_jpg, imgSrc_png } = tabPanes[i];
+      product = new Product(id, type, name, desc, imgSrc_jpg, imgSrc_png);
+      break;
+
+
     }
 
-    console.log("Đã mặc thử sản phẩm có ID: ", productId);
+  }
 
-    document.querySelectorAll(".col-md-4 .background").style.backgroundImage = `url(./../images/background/${product.imgSrc_jpg})`;
+  console.log("Đã mặc thử sản phẩm có ID: ", productId);
 
-    RenderNavPills()
+  document.querySelectorAll(".col-md-4 .background").style.backgroundImage = `url(./../images/background/${product.imgSrc_jpg})`;
+
+  RenderNavPills()
 
 };
-
-
+// tryOnProduct(id)
 
 
 
